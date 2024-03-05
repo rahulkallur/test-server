2f8e455194ac6d2cbd9483651f42efbfa0e55cfe
Rahul Kallur <52211405+rahulkallur@users.noreply.github.com>
2024-03-05 17:12:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/",(req,res)=>{
+   res.send({
+        status:200,
+        code: 200,
+        message: "Success!!!!!, Service is Live!",
+      });
+})
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
